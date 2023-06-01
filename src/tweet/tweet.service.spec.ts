69cd7ca40e51cf28f5cd1162d5db93946a4956ed
tymocoderod <tymocoder@gmail.com>
2023-05-31 21:48:44 -0700

    Some improvements for existing create function for TweetService

--- src/tweet/tweet.service.spec.ts
+++ src/tweet/tweet.service.spec.ts
@@ -1,52 +1,57 @@
-import { Test, TestingModule } from '@nestjs/testing'
-import { TweetService } from './tweet.service'
+import { Test, TestingModule } from '@nestjs/testing';
+import { BadRequestException, NotFoundException } from '@nestjs/common'
+import { TweetService } from './tweet.service';
 
 describe('TweetService', () => {
-  let service: TweetService
+  let service: TweetService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ TweetService ]
-    }).compile()
+      providers: [TweetService],
+    }).compile();
 
-    service = module.get<TweetService>(TweetService)
-  })
+    service = module.get<TweetService>(TweetService);
+  });
 
   it('should be defined', () => {
-    expect(service).toBeDefined()
-  })
+    expect(service).toBeDefined();
+  });
 
   describe('create', () => {
     it('should create tweet', () => {
-      service.tweets = []
-      const payload = 'This is my tweet'
+      service.tweets = [];
+      const payload = 'This is my tweet';
 
-      const tweet = service.create(payload)
+      const tweet = service.create(payload);
 
-      expect(tweet).toBe(payload)
-      expect(service.tweets).toHaveLength(1)
-    })
+      expect(tweet).toBe(payload);
+      expect(service.tweets).toHaveLength(1);
+      expect(service.tweets[0]).toBe(payload);
+    });
 
     it('should prevent empty tweets', () => {
-      const payload = ''
+      const payload = '';
+      const errorMessage = 'Tweet cannot be empty.'
 
-      const tweet = () => {
-        return service.create(payload)
-      }
+      const createTweet = () => {
+        return service.create(payload);
+      };
 
-      expect(tweet).toThrowError()
-    })
+      expect(createTweet).toThrowError(BadRequestException);
+      expect(createTweet).toThrowError(errorMessage);
+    });
 
     it('should prevent tweets over 100 character', () => {
       const payload =
-        'This is a long tweet over 100 characters This is a long tweet over 100 characters This is a long t...'
+        'This is a long tweet over 100 characters This is a long tweet over 100 characters This is a long t...';
+      const errorMessage = 'Tweet too long.'
 
-      const tweet = () => {
-        return service.create(payload)
-      }
+      const createTweet = () => {
+        return service.create(payload);
+      };
 
-      expect(tweet).toThrowError()
-    })
-  })
-
-})
+      expect(createTweet).toThrowError(BadRequestException);
+      expect(createTweet).toThrowError(errorMessage);
+    });
+  });
+});
